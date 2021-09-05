42c112abe313fd330d8ab5266d9abdf8b2762850
Abimaelr <abimaelr.albu@gmail.com>
2021-09-05 01:51:26 -0300

    Square done

--- src/Components/Squares/index.jsx
+++ src/Components/Squares/index.jsx
@@ -7,12 +7,12 @@ function index() {
             <Container>
                 <div className="squareGroup">
                     <div className="col1">
-                        <div></div>
-                        <div></div>
+                        <div>Primeiro Quadrado</div>
+                        <div>Segundo Quadrado</div>
                     </div>
-                    <div className="col2">
-                        <div>
-                            <div>
+                    <div className="center">
+                        <div className="squareTitle">
+                            <div >
                                 <h4>14 anos.</h4>
                                 <h4>Obrigado!</h4>
                             </div>
@@ -21,7 +21,7 @@ function index() {
                                 <p>Faz a tua equipa</p>
                             </div>
                         </div>
-                        <div>
+                        <div className="resume">
                             <p>
                                 O pilar da PrimeIT são pessoas que nela trabalham e o valor que criamos para os nossos clientes. Mais de 14 anos de experiência permitem-nos oferecer as melhores soluções, através de uma equipa de 1900 Primers que nos representam diariamente.
                             </p>
@@ -36,7 +36,23 @@ function index() {
                             </div>
                         </div>
                     </div>
+                   
                 </div>
+            </Container>
+            <Container>
+            <div id="Area">
+                        <h3><em>As nossas áreas de actividade</em></h3>
+                        <div className="line" />
+                        <p>A experiência adquirida nas áreas de actividade onde actuamos, permite-nos fazer face a qualquer tipo de desafio. Conheça o que fazemos.</p>
+                        <div className="icons">
+                            <div>
+                                TELECOM
+                            </div>
+                            <div>IT</div>
+                            <div>ENERGIA</div>
+                            <div>INFRAESTRUTURAS</div>
+                        </div>
+                    </div>
             </Container>
             
         </div>
